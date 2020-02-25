03e10fc896dd1b70ce257ad45a6ea4777ec8174a
bgd988 <zemun88@gmail.com>
2020-02-25 18:39:01 +0100

    kreiranje repozitorijuma

---
+++ src/main/java/main/school_bk/Main.java
@@ -0,0 +1,18 @@
+
+package main.school_bk;
+
+class Main {
+
+  
+    public static void main(String[] args) {
+        
+         System.out.println("Pocetak");
+
+        String message = "Ovo je poruka";
+
+        System.out.println();
+        
+        
+    }
+    
+}
