89e47364a536470ebb8d31d5f61db87f58a318da
bgd988 <zemun88@gmail.com>
2020-02-25 19:58:56 +0100

    napravljena klasa Computer

--- src/main/java/main/school_bk/Computer.java
+++ src/main/java/main/school_bk/Computer.java
@@ -11,4 +11,58 @@ package main.school_bk;
  */
 public class Computer {
     
+    private String operatingSystem = "Linux";
+    private double processTact;
+    private double memory;
+    private int hardDrive;
+    
+    public Computer() {
+        this.operatingSystem = "";
+        this.processTact = 0.0;
+        this.memory = 0.0;
+        this.hardDrive = 0;
+
+    }
+    
+    public Computer(String customoperatingSystem, double customProcessTact, double customMemory,int customHardDrive ) {
+        this.operatingSystem = customoperatingSystem;
+        this.processTact = customProcessTact;
+        this.memory = customMemory;
+        this.hardDrive = customHardDrive;
+
+    }
+    
+    public String getOperatingSystem() {
+        return this.operatingSystem;
+    }
+
+    public void setOperatingSystem(String customoperatingSystem) {
+        this.operatingSystem = customoperatingSystem;
+    }
+    
+    public double getProcessTact() {
+        return this.processTact;
+    }
+
+    public void setProcessTact(double customProcessTact) {
+        this.processTact = customProcessTact;
+    }
+    
+    public double getMemory() {
+        return this.memory;
+    }
+
+    public void setNumberOfClasses(double customMemory) {
+        this.memory = customMemory;
+    }
+    
+      public int getHardDrive() {
+        return this.hardDrive;
+    }
+
+    public void setNumberOfClasses(int customHardDrive) {
+        this.hardDrive = customHardDrive;
+    }
+    
+    
 }
