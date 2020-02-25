b41780f9d281023ef6d295c7e8fad673e4855c6c
bgd988 <zemun88@gmail.com>
2020-02-25 19:07:28 +0100

    napravljena klasa Student

--- src/main/java/main/school_bk/Main.java
+++ src/main/java/main/school_bk/Main.java
@@ -1,18 +1,15 @@
-
 package main.school_bk;
 
 class Main {
 
-  
     public static void main(String[] args) {
-        
-         System.out.println("Pocetak");
 
-        String message = "Ovo je poruka";
+        System.out.println("Pocetak");
 
         System.out.println();
-        
-        
+
+        Student novi = new Student("Bojan ", "Nikolic", 1988);
+        novi.info();
     }
-    
+
 }
--- src/main/java/main/school_bk/Student.java
+++ src/main/java/main/school_bk/Student.java
@@ -1,14 +1,53 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main.school_bk;
 
-/**
- *
- * @author mojme
- */
 public class Student {
-    
+
+    private String firstName = "ime";
+    private String lastName = "prezime";
+    private int yearOfBirth;
+
+    public Student() {
+        this.firstName = "";
+        this.lastName = "";
+        this.yearOfBirth = 0;
+
+    }
+
+    public Student(String customFirstName, String customLastName, int customYearOfBirth) {
+        this.firstName = customFirstName;
+        this.lastName = customLastName;
+        this.yearOfBirth = customYearOfBirth;
+
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String customFirstName) {
+        this.firstName = customFirstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String customLastName) {
+        this.lastName = customLastName;
+    }
+
+    public int getYearOfBirth() {
+        return this.yearOfBirth;
+    }
+
+    public void setYearOfBirth(int customYearOfBirth) {
+        this.yearOfBirth = customYearOfBirth;
+    }
+
+    public void info() {
+        System.out.println("First name:" + this.getFirstName());
+        System.out.println("Last name:" + this.getLastName());
+        System.out.println("Year of birth:" + this.getYearOfBirth());
+
+    }
 }
