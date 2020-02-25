0b8efd730d090d472a1780bd8caacf33b289865c
bgd988 <zemun88@gmail.com>
2020-02-25 19:43:08 +0100

    napravljena klasa Course

--- src/main/java/main/school_bk/Course.java
+++ src/main/java/main/school_bk/Course.java
@@ -11,4 +11,53 @@ package main.school_bk;
  */
 public class Course {
     
+    private String name = "QA";
+    private String codeName = "QA43";
+    private int numberOfClasses;
+    
+    public Course() {
+        this.name = "";
+        this.codeName = "";
+        this.numberOfClasses = 0;
+
+    }
+    
+     public Course( String customName, String customCodeName, int customNumberOfClasses ) {
+        this.name = customName;
+        this.codeName = customCodeName;
+        this.numberOfClasses = customNumberOfClasses;
+
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String customName) {
+        this.name = customName;
+    }
+    
+    public String getCodeName() {
+        return this.codeName;
+    }
+
+    public void setCodeName(String customCodeName) {
+        this.codeName = customCodeName;
+    }
+    
+    public int getNumberOfClasses() {
+        return this.numberOfClasses;
+    }
+
+    public void setNumberOfClasses(int customNumberOfClasses) {
+        this.numberOfClasses = customNumberOfClasses;
+    }
+    
+    
+
+    
+    
+    
 }
+
+  
